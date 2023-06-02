568924ff2c535405c68809bf4bc1d248c7b8c51f
samanthakarnes <samantha.moudry@gmail.com>
2023-06-02 02:12:12 -0500

    Initial commit

---
+++ src/Helper/DateAndTimeProcessing.java
@@ -0,0 +1,12 @@
+package Helper;
+
+import java.sql.Time;
+import java.sql.Date;
+import java.sql.Timestamp;
+
+abstract public class DateAndTimeProcessing {
+    Date createDate;
+    String createdBy;
+    Timestamp lastUpdate;
+    String lastUpdateBy;
+}
---
+++ src/Model/Customers.java
@@ -0,0 +1,14 @@
+package Model;
+
+import Helper.DateAndTimeProcessing;
+
+
+
+public class Customers extends DateAndTimeProcessing {
+    int customerID; //private key in DB
+    String customerName;
+    String address;
+    String postalCode;
+    String phoneNumber;
+    int divisionID; //foreign key in DB
+}
